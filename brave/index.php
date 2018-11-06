a8aada4c94723857a801f2ae094a967e9a7cad50
hoangttpd00983 <tranhoang020237@gmail.com>
2018-11-06 19:47:19 +0700

    Add files via upload

---
+++ brave/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script type="text/javascript" src="boostrap/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Home</title>
+</head>
+<body>
+	<div class="main">
+		<div class="container-fluid">
+			<header>
+				<div class="header">
+					<div class="container">
+						<div class="out-header">
+						<div class="row">
+							<div class="col-md-10">
+								<div class="header-logo">
+									<a href="#"><img src="images/logo.png"></a>
+								</div>
+							</div>
+							<div class="col-md-2">
+								<div class="header-cart">
+									
+								</div>
+							</div>
+						</div>
+					</div>
+					</div>
+				</div>
+				<!------END-Header----->
+			</header>
+		<!------END-Container-fluid----->
+	</div>
+</body>
+</html>
